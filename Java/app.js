c21a1f9958795fe0dcccfdd5bd3b023ecdfb93ef
SangDo.L <126847724+twoupload@users.noreply.github.com>
2023-03-29 16:20:05 +0900

    array, objects
    learn array, objects

--- Java/app.js
+++ Java/app.js
@@ -1,3 +1,10 @@
-const amIFat = null;
-let something;
-console.log(something,amIFat);
+const player = {
+    name:"nico",
+    points: 10,
+    fat: true,
+};
+
+console.log(player);
+player.lastName = "potato";
+player.points= player.points+15;
+console.log(player);
