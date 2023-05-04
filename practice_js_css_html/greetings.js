ae670088abbb0a955561064cb6fb24efbc66bd82
SangDo.L <126847724+twoupload@users.noreply.github.com>
2023-05-05 02:29:45 +0900

    Update greetings.js
    다른 사용법에 대한 주석 추가

--- practice_js_css_html/greetings.js
+++ practice_js_css_html/greetings.js
@@ -1,31 +1,32 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
+
+// const loginInput = document.querySelector(#login-form input);
+// const loginButton = document.querySelector(#login-form button);
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-
-    loginForm.classList.add("HIDDEN_CLASSNAME");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("USERNAME_KEY", username);
+    // greeting.innerText = "Hello " + username;
+    localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
 }
 
-function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
+function paintGreetings() {
+    greeting.innerText = `Hello ${savedUsername}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem("USERNAME_KEY");
-
-console.log(savedUsername);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+    loginForm.addEventListener("submit",onLoginSubmit);
 } else {
     paintGreetings(savedUsername);
 }
