3c75821e184344ee3a411e6109fae37f9be5a1f3
SangDo.L <126847724+twoupload@users.noreply.github.com>
2023-05-09 21:42:41 +0900

    Add files via upload
    배경과 명언에 대한 주석을 달았음.

--- practice_js_css_html/background.js
+++ practice_js_css_html/background.js
@@ -1,11 +1,20 @@
+//배경화면 추가
+
+// 이미지를 import를 한다. 
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 
+// random을 이용해 이미지의 배열에서 이미지를 랜덤으로 뽑아낸다. 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
+// createElement("파일확장자") : javascript에서 파일 확장자를 가져오는 것.
+// javascript에서 html에서 가져오는 것
+// 이미지 파일을 생성하라.
 const bgImage = document.createElement("img");
 
+// 선택된 이미지를 가져온다
 bgImage.src = `img/${chosenImage}`
 
 console.log(bgImage);
 
+// 만든 javascript에서 만든 것을 body에 추가.
 document.body.appendChild(bgImage)
--- practice_js_css_html/quotes.js
+++ practice_js_css_html/quotes.js
@@ -1,8 +1,9 @@
+// 명언 만들기
 const quotes = [
     {
         quote: "With great power comes great responsibility.",
         author: "spiderman",
-    },
+    }, // 콤마(,)로 구분한다.
     {
         quote: "I can do this all day.",
         author: "Captain America",
@@ -39,12 +40,21 @@ const quotes = [
         quote: "Every man dies, not every man really lives.",
         author: "Braveheart",
     },
-];
+]; // 마무리할 때는 ;로 닫아준다. 
 
 const quote = document.querySelector("#quote span:first-child")
+
 const author = document.querySelector("#quote span:last-child")
 
-const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)];
+// 무작위로 명언을 불러오도록 함.
+// Math 모듈을 사용해야 함. 브라우저에 기본 지급
+// Math.random() : 0 부터 1까지의 수 중 아무나
+const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)]; // 가지고 있는 명언의 갯수만큼 더해준다. 
 
-quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+// Math.round() : 반올림 해서 올려준다.
+// Math.floor() : 반을 내려준다.  
+// Math. ceil() : 반올림 하되 전부 올려준다. 
+
+
+quote.innerText = todaysQuote.quote; // html의  quote의 span을 채워준다.
+author.innerText = todaysQuote.author; // html의author의 span을 채워준다. 
